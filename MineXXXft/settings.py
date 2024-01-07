481572d34f321b3833c412e54a677d242a7e4127
ZoronCraft <kazimirkovolodimyr@gmail.com>
2024-01-07 16:24:42 +0200

    CREATION_OF_DJANGO_APPLICATIONS

--- MineXXXft/settings.py
+++ MineXXXft/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_blog'
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TEMPLATE_DIRS = (
+    r'D:\django\myblog' + '/templates/',
+)
